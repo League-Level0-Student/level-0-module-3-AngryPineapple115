72d95ec6444cdced689dd9bc2b4f4e83f7a60134
league <league@wts-im-17.attlocal.net>
2018-04-03 17:02:12 -0700

    HighLowGame RobotColorChooser

--- src/elseif/HighLowGame.java
+++ src/elseif/HighLowGame.java
@@ -5,32 +5,51 @@ package elseif;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class HighLowGame {
 
 	public static void main(String[] args) {
-		// 3. Change this line to give you a random number between 1 - 100. 
-		int random = new Random().nextInt(5);
-		
+		// 3. Change this line to give you a random number between 1 - 100.
+		int random = new Random().nextInt(99) + 1;
+
 		// 2. Print out the random variable above
-		
+		System.out.println(random);
 		// 11. Repeat steps 1 to 10 ten times
-		
-			// 1. Ask the user for a guess using a pop-up window, and save their response 
+		for (int i = 0; i < 10; i++) {
 
+			// 1. Ask the user for a guess using a pop-up window, and save their response
+			String Guess = JOptionPane.showInputDialog("Guess a number from 1-100");
 			// 4. Convert the users’ answer to an int (Integer.parseInt(string))
-			
+			int compare = Integer.parseInt(Guess);
 			// 5. if the guess is correct
-				// 6. Win
-				// 12. Use "System.exit(0);" to quit the game if the user guessed the right answer.
-			// 7. if the guess is high
-				// 8. Tell them it's too high
-			// 9. if the guess is low
-				// 10. Tell them it's too low
+			if (compare == (random))
 
+			{
+
+				// 6. Win
+				JOptionPane.showMessageDialog(null, "You win!");
+				// 12. Use "System.exit(0);" to quit the game if the user guessed the right
+				// answer.
+				System.exit(0);
+			}
+			// 7. if the guess is high
+			else if (compare > random)
+
+			{
+				// 8. Tell them it's too high
+				JOptionPane.showMessageDialog(null, "Too high.");
+			}
+			// 9. if the guess is low
+			else if (compare < random)
+
+			{
+				// 10. Tell them it's too low
+				JOptionPane.showMessageDialog(null, "Too low.");
+			}
+		}
 		// 13. Tell them they lose
-		
+		JOptionPane.showMessageDialog(null, "You lose");
 	}
 
 }
-
-
--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -4,24 +4,51 @@
 package elseif;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
-		//1. Create a new Robot
-		
-		//3. Ask the user what color they would like the robot to draw
-		
-		//5. Use an if/else statement to set the pen color that the user requested
+		// 1. Create a new Robot
+		Robot bob = new Robot();
+		bob.penDown();
+		bob.setSpeed(10);
+		// 3. Ask the user what color they would like the robot to draw
+		for (int i = 0; i < 5; i++) {
 
-        //6. If the user doesn’t enter anything, choose a random color
+			String color = JOptionPane.showInputDialog("Choose a color that you want Bob to draw with.");
+			// 5. Use an if/else statement to set the pen color that the user requested
+			if (color.equals("red")) {
+				bob.setPenColor(Color.RED);
+			} else if (color.equals("orange")) {
+				bob.setPenColor(Color.ORANGE);
+			} else if (color.equals("yellow")) {
+				bob.setPenColor(Color.yellow);
+			} else if (color.equals("green")) {
+				bob.setPenColor(Color.GREEN);
+			} else if (color.equals("blue")) {
+				bob.setPenColor(Color.BLUE);
+			}
+			// 6. If the user doesn’t enter anything, choose a random color
+			if (color.equals("")) {
 
-        //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
-		
-		//4. Set the pen width to 10
-		
-	    //2. Make the robot draw a shape (this will take more than one line of code)
+			}
+			// 7. Put a loop around your code so that you keep asking the user for more
+			// colors & drawing them
 
-
+			// 4. Set the pen width to 10
+			bob.setPenWidth(10);
+			// 2. Make the robot draw a shape (this will take more than one line of code)
+			bob.move(100);
+			bob.turn(90);
+			bob.move(100);
+			bob.turn(90);
+			bob.move(100);
+			bob.turn(90);
+			bob.move(100);
+			bob.turn(90);
+		}
 	}
 }
