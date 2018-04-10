a189635b6c7833daba76f65189c32dcbfd653cc6
loganitis899 <league@wts13.attlocal.net>
2018-04-10 16:57:25 -0700

    ShinyObjects WalkOfFame Bumblebee

--- src/loops/WalkOfFame.java
+++ src/loops/WalkOfFame.java
@@ -4,22 +4,39 @@
 
 package loops;
 
+import java.awt.Color;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class WalkOfFame {
 	public static void main(String[] args) {
 		Robot rob = new Robot();
-		// 1. Set the X position of the robot so that it starts on the left. 
+		// 1. Set the X position of the robot so that it starts on the left.
 		// You also need to show the robot to see the result of this line.
-
+		rob.setX(25);
+		rob.setSpeed(100);
+		rob.penDown();
+		rob.setPenWidth(20);
 		// 2. Make the robot draw a star shape. Hint: 144.
-
+		rob.turn(90);
+		for (int i = 0; i < 8; i++) {
+			for (int e = 0; e < 5; e++) {
+				rob.move(30);
+				rob.turn(-72);
+				rob.move(30);
+				rob.turn(144);
+			}
+			rob.penUp();
+			rob.move(110);
+			rob.setRandomPenColor();
+			rob.penDown();
+		}
+		rob.hide();
 		// 3. Set the size of the star to 30.
 
 		/** THE CHALLENGE: **/
 		/*
-		 * Make the robot draw a line of stars like this:
-		 * http://bit.ly/walk-of-fame
+		 * Make the robot draw a line of stars like this: http://bit.ly/walk-of-fame
 		 * 
 		 * Hint: The distance between stars is 50.
 		 */
