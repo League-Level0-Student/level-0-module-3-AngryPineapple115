702c54cf54b5a09d7228f72a47f0e5eb3f0e96a6
league <league@wts-im-17.attlocal.net>
2018-04-24 17:02:09 -0700

    Test

--- src/modulo/FizzBuzz.java
+++ src/modulo/FizzBuzz.java
@@ -32,7 +32,7 @@ public class FizzBuzz {
 				System.out.println("FizzBuzz");
 			}
 
-			else if (i % 3 == 0) {
+			    else if (i % 3 == 0) {
 				System.out.println("Fizz");
 			} else if (i % 5 == 0) {
 				System.out.println("Buzz");
